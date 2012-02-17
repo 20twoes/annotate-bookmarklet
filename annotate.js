67836d9342277cf5faf4b2f38b66edc11f4273f4
Al Nguyen <al@mediabistro.com>
2012-02-17 10:27:30 -0500

    Rename js file

--- annotate.js
+++ annotate.js
@@ -1,284 +1,296 @@
-var ant = ant || {};
+var Ant = function($) {
+	var self = {};
 
 
-// Logger - interface for local storage
-// ----------------------------------------------------------------------
-ant.newLogger = function(videoSrc) {
-	return {
+	// Our video element.
+	//
+	self.video = (function() {
+		// Get the video we're going to annotate
+		var video = document.getElementsByTagName('video');
+		video = video[0] || null;
 
-		key : 'mediabistro.ant.' + window.location + '.' + videoSrc,
+		return video;
+	})();
 
-		log : function( val ) {
-			window.localStorage.setItem( this.key, JSON.stringify(val) );
-			//console.log(this.key);
-		},
 
-		get : function() {
-			//console.log(this.key);
-			return JSON.parse( window.localStorage.getItem( this.key ) );
+	// Our model for a note.
+	//
+	self.Note = Backbone.Model.extend({
+
+		initialize: function() {
+			var that = this;
+			// Get video time rounded to 2 decimal places.
+			// TODO: Account for delayed reaction time? (by subtracting .5)
+			var getTime = function() {
+				var now = Math.round(self.video.currentTime * 100)/100;
+				// Don't override the time if exists,
+				// like when we first fetch from local storage.
+				return that.get('time') || now;
+			};
+
+			// Set the time the note was taken
+			this.set({time: getTime()});
 		}
-	};
-};
-
-
-// Video Controller - interface for <video> DOM element
-// ----------------------------------------------------------------------
-ant.newVideo = function() {
-	return {
-		el: null,
-
-		select: function() {
-			// Get the video we're going to annotate
-			var video = document.getElementsByTagName('video');
-			//console && console.log(ant.video);
-			video = video[0] || null;
-
-			//console && console.log(video.src);
-
-			return video;
-		},
-
-		getOffset: function(video) {
-			// Get video position in window
-			var offset = $(video).offset();
-			offset.right = offset.left + $(video).width();
-			offset.bottom = offset.top + $(video).height();
-
-			//console && console.log(offset);
-
-			return offset;
-		}
-	};
-};
-
-
-// Main logic
-// ----------------------------------------------------------------------
-ant.main = function($) {
-	console && console.log('main');
-
-	this.video = this.newVideo();
-	this.video.el = this.video.select();
-
-	// Stop if no video!  <-- Exit condition
-	if (!this.video.el) return;
-
-	// Holds the current time of the video
-	this.time = 0;
-
-	// Define storage key
-	this.logger = this.newLogger(this.video.el.src);
-
-	// Need to remove previous assets so we don't accumulate on multiple bookmarklet presses
-	// Remove script 
-	//$("script[src='http://al.mediabistro.net/labs/al/annotate/annotate.js']").remove();
-	// Remove css
-	//$("link[href='http://al.mediabistro.net/labs/al/annotate/annotate.css']").remove();
-
-	// Add CSS styles, if not present
-	// TODO: check if css is present
-	$('head').append('<link href="http://al.mediabistro.net/labs/al/annotate/annotate.css" type="text/css" rel="stylesheet">');
-
-
-	// Build interface __
-
-	this.video.offset = this.video.getOffset(this.video.el);
-	
-	// Toolbar
-	this.toolbar = '<div class="ant-toolbar"><span class="ant-move">[move]</span><span class="ant-close">[x]</span></div>';
-
-	// Input text box
-	if (!$('#ant-input').length) {
-		this.input = $('<div id="ant-input-wrap" class="ant-draggable">' + this.toolbar + '<input type="text" id="ant-input" value="Enter notes!!!" /></div>');
-		$('body').append(this.input);
-		$('#ant-input-wrap').offset({
-			top: this.video.offset.bottom,
-			left: this.video.offset.left
-		});
-	}
-
-	// Display
-	if (!$('#ant-display').length) {
-		this.display = $('<div id="ant-display" class="ant-draggable">' + this.toolbar + '<h2>Notes</h2><ul></ul></div>');
-		$('body').append(this.display);
-		$('#ant-display').offset({
-			top: this.video.offset.top,
-			left: this.video.offset.right
-		});
-	}
-
-	// UI functionality
-	$(function() {
-		$('.ant-draggable').draggable();
 	});
 
 
-	// Notes controller __
+	// Our collection object for notes.
+	//
+	self.NoteList = Backbone.Collection.extend({
+		model: self.Note,
 
-	this.notes = {
+		localStorage: new Store('ant'),
 
-		arr : [],
-		list : $('#ant-display ul'),
+		comparator: function(note) {
+			return note.get('time');
+		}
 
-		add : function(val) {
+	});
 
-			if (!val)
+
+	// Our model view for a note.
+	//
+	self.NoteView = Backbone.View.extend({
+		tagName: 'li',
+
+		template: _.template(
+			'<div class="ant-note-display">\
+				<a href="#" class="ant-note-link" data-time="<%= time %>" title="<%= time %> sec"><%= text %></a>\
+				________<span class="ant-note-edit-button">[Edit]</span><span class="ant-note-destroy">[Delete]</span>\
+			</div>\
+			<div class="ant-note-edit" style="display:none;">\
+				<input class="ant-note-input" type="text" value="" />\
+			</div>'
+		),
+
+		events: {
+			'click .ant-note-destroy': 'clear',
+			'click .ant-note-link': 'updateVideo',
+			'click .ant-note-edit-button': 'edit',
+			'keypress .ant-note-input': 'updateOnEnter'
+		},
+
+		initialize: function() {
+			this.model.bind('change', this.render, this);
+			this.model.bind('destroy', this.remove, this);
+
+		},
+
+		render: function() {
+			$(this.el).html(this.template(this.model.toJSON()));
+			this.setText();
+			return this;
+		},
+
+		setText: function() {
+			var text = this.model.get('text');
+			this.$('.ant-note-link').text(text);
+			this.input = this.$('.ant-note-input');
+			this.input.bind('blur', _.bind(this.close, this)).val(text);
+		},
+
+		edit: function() {
+			//$(this.el).addClass('editing');
+			this.$('.ant-note-display').hide();
+			this.$('.ant-note-edit').show();
+			this.input.focus();
+			return false;
+		},
+
+		close: function() {
+			this.model.save({text: this.input.val()});
+			//$(this.el).removeClass('editing');
+			this.$('.ant-note-display').show();
+			this.$('.ant-note-edit').hide();
+		},
+
+		updateOnEnter: function(e) {
+			if (e.keyCode == 13)
+				this.close();
+		},
+
+		remove: function() {
+			$(this.el).remove();
+		},
+
+		clear: function() {
+			this.model.destroy();
+		},
+
+		updateVideo: function() {
+			self.video.currentTime = this.model.get('time');
+		}
+	});
+
+
+	// Our application view.
+	//
+	self.AppView = Backbone.View.extend({
+
+		template: _.template(
+			'<h1>Notes</h1>\
+			<div class="ant-content">\
+				<div id="ant-create-note">\
+					<input id="ant-new-note" placeholder="Enter a note about the video" type="text" />\
+					<span class="ant-ui-tooltip-top" style="display:none;">Press Enter to save this note</span>\
+				</div>\
+				<div id="ant-notes">\
+					<ul id="ant-note-list"></ul>\
+				</div>\
+			</div>'
+		),
+
+		events: {
+			'keypress #ant-new-note': 'createOnEnter',
+			'keyup #ant-new-note': 'showTooltip'
+		},
+
+		initialize: function() {
+			self.Notes.bind('add', this.addOne, this);
+			self.Notes.bind('reset', this.addAll, this);
+
+			// Must create #ant-new-note in render() before using it
+			this.render();
+			this.input = this.$('#ant-new-note');
+
+			// Fetch after rendering
+			self.Notes.fetch();
+		},
+
+		render: function() {
+			$(this.el).html(this.template());
+			return this;
+		},
+
+		addOne: function(note) {
+			var view = new self.NoteView({model: note});
+			this.$('#ant-note-list').append(view.render().el);
+		},
+
+		addAll: function() {
+			self.Notes.each(this.addOne);
+		},
+
+		createOnEnter: function(e) {
+			var text = this.input.val();
+			if (!text || e.keyCode != 13)
 				return;
-
-			var o = { note: val, time: ant.time };
-			this.arr.push(o);
-			//console.log(this.arr);
-			this.arr.sort( function(a,b) {
-				return (a.time - b.time);
-			});
-			this.refresh();
+			self.Notes.create({text: text});
+			this.input.val('');
 		},
 
-		updateList : function (i) {
-			if (i == null) {
-				i = this.arr.length - 1;
-			}
-			var o = this.arr[i];
+		showTooltip: function(e) {
+			var tooltip = this.$('.ant-ui-tooltip-top');
+			var val = this.input.val();
+			tooltip.fadeOut();
+			if (this.tooltipTimeout)
+				clearTimeout(this.tooltipTimeout);
+			if (val == '' || val == this.input.attr('placeholder'))
+				return;
+			var show = function() {
+				tooltip.show().fadeIn();
+			};
+			this.tooltipTimeout = _.delay(show, 1000);
+		}
+	});
 
-			//console.log(o);
 
-			var item = '<a href="#rm" class="ant-note-rm" data-id="' + i + '" title="Delete">[x]</a> :: <a href="#" class="ant-note-link" data-time="' + o.time + '" title="' + o.time + ' sec">' + o.note + '</a>';
-			this.list.append( '<li>' + item + '</li>' );
-		},
+	// Create Notes collection
+	self.Notes = new self.NoteList;
 
-		refresh : function () {
-			this.list.empty();
-
-			//console.log(this.list.html());
-
-			for ( var i = 0; i < this.arr.length; ++i ) {
-				this.updateList( i );
-			}
-		},
-
-		delete : function(i) {
-			this.arr.splice( i, 1 );
+	// Create encompassing div first
+	var el = (new Backbone.View).make('div', {id: 'ant-app', class: 'ant-draggable'});
+	$(el).offset(function() {
+		// Get video position in window
+		var offset = self.video && $(self.video).offset();
+		if (offset) {
+			offset.left += $(self.video).width() + 12;  // plus padding
+			//offset.top += $(self.video).height() + 50;
+		} else {
+			offset = { top: 0, left: 0 };
 		}
 
+		return offset;
+	});
+	$('body').append(el);
+	$('.ant-draggable').draggable();
+
+	// Inject app into new div
+	self.App = new self.AppView({el: el});
+
+	return self;
+};
+
+
+function AntLoad() {
+
+	// __Load dependencies
+	var dependencies = [
+		{
+			obj: 'jQuery',
+			src: 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js'
+		},
+		{
+			obj: 'jQuery.ui',
+			src: 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js'
+		},
+		{
+			obj: '_',
+			src: 'http://documentcloud.github.com/underscore/underscore-min.js'
+		},
+		{
+			obj: 'Backbone',
+			src: 'http://documentcloud.github.com/backbone/backbone-min.js'
+		},
+		{
+			obj: 'Store',
+			src: 'https://raw.github.com/jeromegn/Backbone.localStorage/master/backbone.localStorage.js'
+		},
+		{
+			obj: 'antPlaceholder',
+			src: 'http://raw.github.com/20twoes/annotate-bookmarklet/annotate.css',
+			type: 'css'
+		}
+	];
+
+	var assetID = 1;
+	var load = function(src, type) {
+		if (type) {
+			var el = document.createElement('link');
+			el.type = 'text/css';
+			el.rel = "stylesheet";
+			el.href = src;
+		} else {
+			var el = document.createElement('script');
+			el.type = 'text/javascript';
+			el.src = src;
+		}
+		el.id = 'ant-asset' + assetID++;
+		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(el);
 	};
 
+	for (var i = 0; i < dependencies.length; ++i) {
+		var o = dependencies[i];
+		try {
+			if (eval(o.obj) == undefined)
+				throw o.obj + " is undefined.";
+		} catch(e) {
+			// Load if obj not defined
+			load(o.src, o.type);
+		}
+	}
 	
-	// Attach event handlers __
 
-	// init text input field
-	$('#ant-input')
-		.keypress( function( pEvent ) {
-			var th = $(this);
-
-			// use the time when you start typing instead of when you hit enter
-			// minus a second 
-			if ( !th.val() ) {
-				ant.time = ant.video.el.currentTime - 1;
-			}
-
-			if ( pEvent.which == '13' ) {
-				ant.notes.add( th.val() );
-				th.val('');
-			}
-		})
-		.focus( function() {
-			var th = $(this);
-			th.val('');
-		});
-
-	$('.ant-note-link').live('click', function(e) {
-		var t = $(this);
-		//console.log( t.attr('data-time'));
-		ant.video.el.currentTime = t.attr('data-time');
-		e.preventDefault();
-	});
-
-	$('.ant-note-rm').live('click', function(e) {
-		var t = $(this);
-		ant.notes.delete( t.attr('data-id') );
-		t.parent().remove();
-		e.preventDefault();
-	});
-
-	$(window).unload( function() {
-		ant.logger.log( ant.notes.arr );
-	});
-
-	$('.ant-close').live('click', function(e) {
-		$('#ant-display').remove();
-		$('#ant-input-wrap').remove();
-		e.preventDefault();
-	});
-
-
-	// Highlight note when video time matches __
-
-	this.highlightNote = function() {
-		var t = parseInt(this.video.el.currentTime);
-		var links = $('.ant-note-link');
-		var resetHighlight = function() {
-			$('.ant-active').removeClass('ant-active');
-		};
-		links.each( function() {
-			var th = $(this);
-			if ( t == parseInt(th.attr('data-time')) ) {
-				resetHighlight();
-				th.parent().addClass('ant-active');
-			}
-		});
-		setTimeout('ant.highlightNote();', 500);
-	};
-
-
-	// Init __
-
-	$(document).ready( function() {
-
-		// Load notes from local storage
-		if (ant.notes) {
-			ant.notes.arr = ant.logger.get() || [];
-			ant.notes.refresh();
-		}
-
-		// Run highlighting engine
-		ant.highlightNote();
-	});
-
+	// Start when our last script loads
+	// This is a temp solution
+	// Does not ensure that all scripts have loaded
+	document.getElementById('ant-asset' + (assetID - 2)).addEventListener('load', AntMain);
 };
 
 
-// Dependencies
-// ----------------------------------------------------------------------
-ant.load = function(id, src) {
-	var el = document.createElement('script');
-	el.type = 'text/javascript';
-	el.src = src;
-	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(el);
-	ant[id] = el;
-};
-ant.loadJqueryUI = function() {
-	var main = function() {
-		ant.main(jQuery);
-	};
-
-	if (typeof jQuery.ui == 'undefined') {
-		ant.load( 'jqueryui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js' );
-		ant.jqueryui.addEventListener('load', main);
-	} else {
-		main();
-	}
+function AntMain() {
+	window.ant = new Ant(jQuery);
 };
 
 
-// Bootleg
-// ----------------------------------------------------------------------
-ant.bootleg = function() {
-	if (typeof jQuery == 'undefined') {
-		ant.load( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js' );
-		ant.jquery.addEventListener('load', ant.loadJqueryUI);
-	} else {
-		ant.loadJqueryUI();
-	}
-};
-
-ant.bootleg();
+AntLoad();
